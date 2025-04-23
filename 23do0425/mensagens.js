7ebd8e3cd03759fbda745b6eff99bb008f26fd43
Aluno IFAL <jgca1@aluno.ifal.edu.br>
2025-04-23 14:25:00 -0300

    Adicionando a função IdAleatório no mensagens.js

--- 23do0425/mensagens.js
+++ 23do0425/mensagens.js
@@ -8,5 +8,9 @@ function despedida(nome) {
     return `Até mais ${nome}!`
 }
 
+function gerarIdAleatorio(nome) {
+    return Math.random().toString(36).substring(2,15) /* Gera um número aleatório para a sring da função, que no caso é nome*/
+}
+
 /* para exportar usamos "Module.exports", com o objetivo de que outro javascript possa usar as funções criadas nesse arquivo */
-module.exports = {saudacao, despedida}
+module.exports = {saudacao, despedida, gerarIdAleatorio}
