dc5f1fb55a760ae544689c521dc326a4b7bc90a9
Aluno IFAL <jgca1@aluno.ifal.edu.br>
2025-04-23 14:28:49 -0300

    Executando a function gerarIdAleatorio

--- 23do0425/ms01.js
+++ 23do0425/ms01.js
@@ -2,3 +2,4 @@
 const msg = require("./mensagens") /** Temos que informar o local exato do arquivo em que está o module.exports, é bem parecido quando usamos o cmd */
 console.log(msg.saudacao("José"))
 console.log(msg.despedida("Pedro"))
+console.log(msg.gerarIdAleatorio("José"))
