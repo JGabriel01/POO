b5384548643761dfd4b8383ee364086a2f10bbc1
José Gabriel Cavalcante de Almeida <jgca1@aluno.ifal.edu.br>
2025-08-13 08:10:34 -0300

    create 922A-POO/revisaoInpr/Q3.js

---
+++ 922A-POO/revisaoInpr/Q3.js
@@ -0,0 +1,9 @@
+function calcularMedia(notas) {
+let soma = 0
+for (let i = 0; i < notas.length; i++) {
+ soma += notas[i]
+}
+let media = soma/notas.length
+return media
+}
+console.log(calcularMedia([8,7,9,10,6]))
