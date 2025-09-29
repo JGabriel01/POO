5cf460150b6e2b042cd642ed1ed99d83127e62c6
Aluno IFAL <alunoifal@MACEIO.IFAL>
2025-09-29 17:58:26 -0300

    exercicio 5 resolvido

--- 922A-POO/listaExercicios/q5.js
+++ 922A-POO/listaExercicios/q5.js
@@ -1,2 +1,14 @@
 let cor = "azul"
 
+function mod(color) {
+    let cor2 = color
+    console.log(`-- Antes da modificação --`)
+    console.log(`Cor 1: ${color}`)
+    console.log(`Cor 2: ${cor2}`)
+
+    color = "Preto"
+    console.log(`-- Depois da modificação --`)
+    console.log(`Cor 1: ${color}`)
+    console.log(`Cor 2: ${cor2}`)
+}
+mod(cor)
